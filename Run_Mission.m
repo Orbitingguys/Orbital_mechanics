8f4211257424b4e042ebb497692077108da221e6
PI3R95 <32986447+PI3R95@users.noreply.github.com>
2017-12-16 16:18:34 +0100

    4.1
    It can be a start

--- Run_Mission.m
+++ Run_Mission.m
@@ -49,8 +49,9 @@ mi_saturn = astroConstants(16);
 t_a_max_1st = T0_1st+3*T_jupiter+T_venus;
 % DateSetup.date_a_max = mjd20002date(t_a_max./(3600*24));
 
-m = 100;
-n = 300;
+m = 10;
+n = 30;
+u = 60;
 
 
 legtype_1st = 1;
@@ -75,7 +76,7 @@ end
 
 
 [D_v_2nd,t_d_2nd,t_a_2nd,tof_2nd,X_2_2nd,X_3_2nd,v_transf_2nd_1,v_transf_2nd_2] = pork_chopLEG(X_2_0_2nd,X_3_0_2nd,...
-    t_d_2nd(1),t_d_2nd(end),t_a_min_2nd,t_a_max_2nd,mi_sun,n,600,2);
+    t_d_2nd(1),t_d_2nd(end),t_a_min_2nd,t_a_max_2nd,mi_sun,n,u,2);
 
 D_v_2nd = D_v_2nd';
 
@@ -83,20 +84,40 @@ D_v_2nd = D_v_2nd';
 
 %% FLY BY
 
-% D_v_FB = zeros(n);
-for i = 2:n
-    [~,X_Flyby] = ode113(@orbit_dynamics,linspace(0,t_d_2nd(i),3),X_2_0_1st,options,mi_sun);
-    R_Flyby = X_Flyby(end,1:3); V_Flyby = X_Flyby(end,4:6);
-    orbital_parameters_planet = geo2kep(R_Flyby,V_Flyby,mi_sun);
+D_v_fby = zeros(m,u,n);
+mjd2000_0_2nd = zeros(n,1);
+
+for i = 1:n
+    mjd2000_0_2nd(i) = t_d_2nd(i)/(60*60*24);
+    orbital_parameters_jupiter_2nd = OrbitalParameters(mjd2000_0_2nd(i),body_2nd);
     for j = 1:m
-        v_1st_2 = [v_transf_1st_2(j,i,1);v_transf_1st_2(j,i,2);v_transf_1st_2(j,i,3)];
-        v_2nd_1 = [v_transf_2nd_1(i,k(i),1);v_transf_2nd_1(i,k(i),2);v_transf_2nd_1(i,k(i),3)];
-        if (norm(v_1st_2) && norm(v_2nd_1)) > 0
-            [~, ~, D_v_FB(i,j),deltaPA]  =  poweredflyby (v_1st_2,v_2nd_1,orbital_parameters_planet,mi_jupiter);
+        for w = 1:k
+            v_1 = [v_transf_1st_2(j,i,1); v_transf_1st_2(j,i,2); v_transf_1st_2(j,i,3)];
+            v_2 = [v_transf_2nd_1(i,w,1); v_transf_2nd_1(i,w,2); v_transf_2nd_1(i,w,3)];
+            [~, ~, delta_v]  =  poweredflyby (v_1, v_2,orbital_parameters_jupiter_2nd,mi_jupiter);
+            D_v_fby(j,w,i) = delta_v;
         end
-        
     end
 end
+
+mD_v_fby = min(D_v_fby);
+mmD_v_fby = min(mD_v_fby);
+min_D_v_fby = min(mmD_v_fby)
+
+% D_v_FB = zeros(n);
+% for i = 2:n
+%     [~,X_Flyby] = ode113(@orbit_dynamics,linspace(0,t_d_2nd(i),3),X_2_0_1st,options,mi_sun);
+%     R_Flyby = X_Flyby(end,1:3); V_Flyby = X_Flyby(end,4:6);
+%     orbital_parameters_planet = geo2kep(R_Flyby,V_Flyby,mi_sun);
+%     for j = 1:m
+%         v_1st_2 = [v_transf_1st_2(j,i,1);v_transf_1st_2(j,i,2);v_transf_1st_2(j,i,3)];
+%         v_2nd_1 = [v_transf_2nd_1(i,k(i),1);v_transf_2nd_1(i,k(i),2);v_transf_2nd_1(i,k(i),3)];
+%         if (norm(v_1st_2) && norm(v_2nd_1)) > 0
+%             [~, ~, D_v_FB(i,j),deltaPA]  =  poweredflyby (v_1st_2,v_2nd_1,orbital_parameters_planet,mi_jupiter);
+%         end
+%         
+%     end
+% end
 
 %% DEPARTURE FROM VENUS
 
@@ -105,4 +126,4 @@ end
 %% BEST DELTA V
 
 
-D_v = D_v_1st + min_D_v_2;
+% D_v = D_v_1st + min_D_v_2;
